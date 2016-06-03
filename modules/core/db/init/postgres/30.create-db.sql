1cbefa0a6ae4dcf4927b69d7d7cf6ad2723041f7
Yuriy Artamonov <artamonov@haulmont.com>
2016-06-03 16:05:15 +0400

    Dashboard sample on HSQL

--- modules/core/db/init/postgres/30.create-db.sql
+++
@@ -1,4 +0,0 @@
-insert into SEC_USER (ID, CREATE_TS, VERSION, LOGIN, LOGIN_LC, PASSWORD, NAME, GROUP_ID, ACTIVE)
-values ('15672452-c835-dcd8-1bdb-35efcdefefda', now(), 0, 'demo', 'demo',
-'c956de0f1cb49c302777ded5dd8bf85848b21ca1',
-'Demo', '0fa2b1a5-1d68-4d69-9fbd-dff348347f93', true);
